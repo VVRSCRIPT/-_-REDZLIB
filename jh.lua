526e392e4d35add53934bd5a68ddaa356cc07ddb
A_IDK313 <malkhateeb919@gmail.com>
2026-02-23 18:14:24 +0300

    Mm

---
+++ jh.lua
@@ -0,0 +1,21 @@
+loadstring(game:HttpGet(("https://raw.githubusercontent.com/Seven7-lua/RedzLibs/refs/heads/main/src/RedzlibV2/source.lua")))()
+
+MakeWindow({
+  Hub = {
+    Title = "REDz HUB",
+    Animation = "by : redz9999"
+  },
+  Key = {
+    KeySystem = false,
+    Title = "Key System",
+    Description = "",
+    KeyLink = "",
+    Keys = {"1234"},
+    Notifi = {
+      Notifications = true,
+      CorrectKey = "Running the Script...",
+      Incorrectkey = "The key is incorrect",
+      CopyKeyLink = "Copied to Clipboard"
+    }
+  }
+})
