1bba42d5637e6a6bcc7558ccaf9d15c4030eadd2
A_IDK313 <malkhateeb919@gmail.com>
2026-02-23 18:29:39 +0300

    Nm

--- jh.lua
+++ jh.lua
@@ -1,11 +1,8 @@
-local redzlib = loadstring(game:HttpGet("https://raw.githubusercontent.com/Seven7-lua/RedzLibs/refs/heads/main/src/RedzlibV3/source.lua"))()
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/Seven7-lua/RedzLibs/refs/heads/main/src/RedzlibV4/source.lua"))()
 
-Menu = {
-    Title = "REDz HUB teste",
-  },
-  LoadAnim = {
-    Active = true,
-    Title = "by : redz9999",
-    WaitTime = 0.5
-  }
+local Window = redzlib:MakeWindow({
+  Title = "REDz HUB : Example",
+  SubTitle = "by : redz9999",
+  LoadText = "redz Hub",
+  Flags = "redz Hub | Example.lua"
 })
